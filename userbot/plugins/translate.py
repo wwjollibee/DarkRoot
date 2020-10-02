d043a91d47e6386ebf42d3bea34b9bafbf1c60a7
DarkUserBot-Team <71947446+DarkUserBot-Team@users.noreply.github.com>
2020-10-02 18:29:56 +0400

    Update translate.py

--- userbot/plugins/translate.py
+++ userbot/plugins/translate.py
@@ -35,7 +35,7 @@ async def _(event):
         after_tr_text = translated.text
         # TODO: emojify the :
         # either here, or before translation
-        output_str = """**DarkUserBot İlə Tərcümə Edildi.** 
+        output_str = """**Dark UserBot İlə Tərcümə Edildi.** 
          Source **( {} )**
          Translation **( {} )**
          {}""".format(

8b39695e27a6578725f1589b861d4b6bce125739
DarkUserBot-Team <71947446+DarkUserBot-Team@users.noreply.github.com>
2020-10-05 17:58:59 +0400

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -29,7 +29,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Dark plugin menusu !"""
+            help_string = """DarkUB plugin menusu !"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
             )

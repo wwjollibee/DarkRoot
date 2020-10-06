0a1e4bd9a966978bbe60a7ede240a8d0757f7ad9
DarkUserBot-Team <71947446+DarkUserBot-Team@users.noreply.github.com>
2020-10-06 20:39:17 +0400

    Update install.py

--- userbot/plugins/install.py
+++ userbot/plugins/install.py
@@ -22,14 +22,14 @@ async def install(event):
                 shortname = path1.stem
                 load_module(shortname.replace(".py", ""))
                 await event.edit(
-                    "Dark Plugin. `{}` Yükləndi.".format(
+                    "Dark Plugin: `{}` yükləndi.".format(
                         os.path.basename(downloaded_file_name)
                     )
                 )
             else:
                 os.remove(downloaded_file_name)
                 await event.edit(
-                    "Errors! This plugin is already installed/pre-installed."
+                    "Xəta! Bu plugin yüklüdür!."
                 )
         except Exception as e:  # pylint:disable=C0103,W0703
             await event.edit(str(e))
